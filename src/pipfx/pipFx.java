c65086e40bab9fc5beb26dda10aede53737b4cc6
maas0903 <4244681+maas0903@users.noreply.github.com>
2017-08-18 22:21:54 +0200

    Initial checkin
    Initial checkin

--- src/pipfx/pipFx.java
+++ src/pipfx/pipFx.java
@@ -9,11 +9,6 @@ package pipFx;
  *
  * @author Marius
  */
-import com.sun.mail.smtp.SMTPTransport;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.InetSocketAddress;
-import java.net.Proxy;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.BufferedReader;
@@ -27,14 +22,9 @@ import java.util.TimerTask;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import static melektro.Email.EMailSender;
+import static melektro.PublicIPAddress.Log;
+import static melektro.PublicIPAddress.GetPublicIp;
 
 public class pipFx {
 
@@ -49,46 +39,6 @@ public class pipFx {
     static boolean bTestMode;
     static String RemotePublicIpNameForEmail;
     static String EmailAddressCC;
-
-    public static void Log(String message) {
-        System.out.println(message);
-    }
-
-    public static boolean EMailSender(String host, String port, final String from, final String pass, String to, String cc, String sub, String mess) {
-        Properties props = System.getProperties();
-        props.setProperty("mail.smtp.auth", "true");
-        props.setProperty("mail.smtp.starttls.enable", "true");
-        props.setProperty("mail.smtp.host", host);
-        props.setProperty("mail.smtp.port", port);
-        Authenticator auth;
-        auth = new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(from, pass);
-            }
-        };
-        try {
-            Session session = Session.getInstance(props, auth);
-            //session.setDebug(verbose);
-            Transport tr;
-            tr = session.getTransport("smtp");
-            if (!(tr instanceof SMTPTransport)) {
-                Log("This is NOT an SMTPTransport:[" + tr.getClass().getName() + "]");
-            }
-            Message message;
-            message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-            message.setRecipients(Message.RecipientType.CC, InternetAddress.parse(cc));
-            message.setSubject(sub);
-            message.setText(mess);
-            Transport.send(message);
-            return true;
-        } catch (MessagingException e) {
-            Log("Exception in EMailSender:" + e.getMessage());
-            return false;
-        }
-    }
 
     public static void sendMail(String Ip) {
         GetProperties();
@@ -116,41 +66,6 @@ public class pipFx {
             }
         }
 
-    }
-
-    private static String GetPublicIp() throws Exception {
-        URL whatismyip = new URL("http://checkip.amazonaws.com/");
-        URLConnection connection;
-        if (ProxyToUse.isEmpty()) {
-            connection = whatismyip.openConnection();
-        } else {
-            Log("  proxy=" + ProxyToUse);
-            Log("Using proxy");
-            Log("  port=" + ProxyPortToUse);
-            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(ProxyToUse, Integer.parseInt(ProxyPortToUse)));
-            connection = whatismyip.openConnection(proxy);
-        }
-        connection.addRequestProperty("Protocol", "Http/1.1");
-        connection.addRequestProperty("Connection", "keep-alive");
-        connection.addRequestProperty("Keep-Alive", "1000");
-        connection.addRequestProperty("User-Agent", "Web-Agent");
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-        String ip = in.readLine();
-        return ip;
-    }
-
-    private static String readFromInputStream(InputStream inputStream) throws Exception {
-        StringBuilder resultStringBuilder = new StringBuilder();
-        try (BufferedReader br
-                = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                resultStringBuilder.append(line);
-            }
-        }
-        return resultStringBuilder.toString();
     }
 
     private static String GetDummyPublicIp() throws Exception {
@@ -298,7 +213,7 @@ public class pipFx {
             if (bTestMode) {
                 publicIpAddress = GetDummyPublicIp();
             } else {
-                publicIpAddress = GetPublicIp();
+                publicIpAddress = GetPublicIp(ProxyToUse, ProxyPortToUse);
             }
             String fileIpAddress = GetFileIp(publicIpAddress);
             if (!fileIpAddress.equals(publicIpAddress)) {
@@ -315,8 +230,8 @@ public class pipFx {
     private static void FileCompare() {
         File file = new File("setpole.txt");
         if (file.exists()) {
-            CompareIp(true);
-            file.delete();
+            CompareIp(false);
+            //file.delete();
         }
     }
 
